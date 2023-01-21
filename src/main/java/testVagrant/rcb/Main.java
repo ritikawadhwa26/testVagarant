20c1145e53b9b0e67ee2cb36c4c60e58c7e8a0c1
Ritika <ritikawadhwa26@gmail.com>
2023-01-21 14:07:09 +0530

    pojo json mapping

--- src/main/java/testVagrant/rcb/Main.java
+++ src/main/java/testVagrant/rcb/Main.java
@@ -1,13 +1,35 @@
 package testVagrant.rcb;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import testVagrant.rcb.pojo.Rcb;
 
 /**
  * Hello world!
  *
  */
-public class Main 
-{
-    public static void main( String[] args ) {
+public class Main {
+	public static void main(String[] args) {
+	}
 
-    }
+	public Rcb getTeam() {
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		// JSON from file to Object
+		Rcb team = null;
+		try {
+			team = mapper.readValue(new File(
+					System.getProperty("user.dir") + File.separator + "resources" + File.separator + "TeamRCB.json"),
+					Rcb.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return team;
+	}
+
 }

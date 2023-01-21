c8c3552f0e36df9d73b5e4cf0d8874956f757b55
Ritika <ritikawadhwa26@gmail.com>
2023-01-21 14:49:52 +0530

    foreign player test initiated

--- src/test/java/testVagrant/rcb/MainTest.java
+++ src/test/java/testVagrant/rcb/MainTest.java
@@ -3,18 +3,35 @@ package testVagrant.rcb;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.testng.annotations.BeforeMethod;
+
+import testVagrant.rcb.pojo.Rcb;
 
 /**
  * A simple unit test
  */
-public class MainTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class MainTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	Rcb rcbTeam;
+
+	@BeforeMethod
+	public void getRcbTeam() {
+		Main m = new Main();
+		rcbTeam = m.getTeam();
+	}
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		System.out.println(rcbTeam.getName());
+	}
+	
+	@Test
+	public void teamHasOnlyFourForeignPlayers()
+	{
+		
+		
+	}
+	
 }
---
+++ src/test/java/testVagrant/rcb/methods/PlayerHelper.java
@@ -0,0 +1,14 @@
+package testVagrant.rcb.methods;
+
+import testVagrant.rcb.pojo.Rcb;
+
+public class PlayerHelper {
+	
+	public Integer getForeignPlayers(Rcb team) {
+		
+		
+		return null;
+		
+	}
+
+}
